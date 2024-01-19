524eb5094aac6ce8ed4cdd7efe69b67e0612877c
desipossa <c1jang@hanmail.net>
2024-01-19 13:19:07 +0900

    20240119

--- 개발/bsc/sub.tail.php
+++ 개발/bsc/sub.tail.php
@@ -22,11 +22,3 @@
                 $('.lnb>ul>li').eq(num-1).addClass('on');
             })
         </script>
-
-
-
-<?php echo $_GET['bo_table']?>
-
-<!-- 우선 url 은 'parameter.php?name=철수' 이다.
-이때 name 의 값을 가져오는 방법은 다음과 같다.
-안녕하세요 <?php echo $_GET['name']?>님 -->
